6832e9afd0a920edc1b6f6a8f6a0191644cb6e39
Alon Zakai <alonzakai@gmail.com>
2012-03-02 16:24:24 -0800

    two databases in test.js

--- test.js
+++ test.js
@@ -1,28 +1,43 @@
 // js -m -n -e "load('sql.js')" test.js
 
-var db = SQL.open();
+function test() {
+  var db = SQL.open();
 
-print('created db');
+  print('created db');
 
-db.exec("CREATE TABLE my_table(a INTEGER, b INTEGER, c VARCHAR(100));");
+  db.exec("CREATE TABLE my_table(a INTEGER, b INTEGER, c VARCHAR(100));");
 
-print('executed one command');
+  print('executed one command');
 
-db.exec("INSERT INTO my_table VALUES(1,13153,'thirteen thousand one hundred fifty three');");
-db.exec("INSERT INTO my_table VALUES(1,987,'some other number');");
+  db.exec("INSERT INTO my_table VALUES(1,13153,'thirteen thousand one hundred fifty three');");
+  db.exec("INSERT INTO my_table VALUES(1,987,'some other number');");
 
-function report(data) {
-  print(JSON.stringify(data, null, '  '));
+  function report(data) {
+    print(JSON.stringify(data, null, '  '));
+  }
+
+  db.exec("SELECT count(*) FROM my_table;", report);
+  // prints [{ "column": "count(*)", "value": "2" }]
+
+  print('printed one report');
+
+  var db2 = SQL.open();
+  try {
+    db2.exec("SELECT a, b, c FROM my_table;", report);
+  } catch(e) {
+    // Failure is expected, as the other db doesn't have that table!
+    db2.close();
+
+    db.exec("SELECT a, b, c FROM my_table;", report);
+    // prints [{ "column": "a", "value": "1" }, { "column": "b", "value": "13153" }, { "column": "c", "value": "thirteen thousand one hundred fifty three" }]
+    //        [{ "column": "a", "value": "1" }, { "column": "b", "value": "987"   }, { "column": "c", "value": "some other number" }]
+
+    db.close();
+    return;
+  }
+
+  throw 'reading from the second db should have failed!';
 }
 
-db.exec("SELECT count(*) FROM my_table;", report);
-// prints [{ "column": "count(*)", "value": "2" }]
-
-print('printed one report');
-
-db.exec("SELECT a, b, c FROM my_table;", report);
-// prints [{ "column": "a", "value": "1" }, { "column": "b", "value": "13153" }, { "column": "c", "value": "thirteen thousand one hundred fifty three" }]
-//        [{ "column": "a", "value": "1" }, { "column": "b", "value": "987"   }, { "column": "c", "value": "some other number" }]
-
-db.close();
+test();
 
