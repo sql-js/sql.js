4f505c75bf458021deab94e73e8e3546c4fbdca8
Alon Zakai <alonzakai@gmail.com>
2012-03-06 14:55:17 -0800

    make test more automatic

--- test.js
+++ test.js
@@ -1,26 +1,20 @@
 // js -m -n -e "load('sql.js')" test.js
 
-function test() {
+function jsonCompare(x, y) {
+  return JSON.stringify(x) == JSON.stringify(y);
+}
+
+function check(data, expected) {
+  if (!jsonCompare(data, expected)) throw 'Comparison failed: ' + JSON.stringify(data) + ' vs. ' + JSON.stringify(expected) + ' at ' + new Error().stack;
+}
+
+function testBasics() {
   var db = SQL.open();
 
-  print('created db');
-
   db.exec("CREATE TABLE my_table(a INTEGER, b INTEGER, c VARCHAR(100));");
-
-  print('executed one command');
-
   db.exec("INSERT INTO my_table VALUES(1,13153,'thirteen thousand one hundred fifty three');");
   db.exec("INSERT INTO my_table VALUES(1,987,'some other number');");
-
-  function report(data) {
-    print(JSON.stringify(data, null, '  '));
-  }
-
-  report(db.exec("SELECT count(*) FROM my_table;"));
-
-  // prints [{ "column": "count(*)", "value": "2" }]
-
-  print('printed one report');
+  check(db.exec("SELECT count(*) FROM my_table;"), [[{ "column": "count(*)", "value": "2" }]]);
 
   var db2 = SQL.open();
   try {
@@ -29,9 +23,10 @@ function test() {
     // Failure is expected, as the other db doesn't have that table!
     db2.close();
 
-    report(db.exec("SELECT a, b, c FROM my_table;"));
-    // prints [{ "column": "a", "value": "1" }, { "column": "b", "value": "13153" }, { "column": "c", "value": "thirteen thousand one hundred fifty three" }]
-    //        [{ "column": "a", "value": "1" }, { "column": "b", "value": "987"   }, { "column": "c", "value": "some other number" }]
+    check(db.exec("SELECT a, b, c FROM my_table;"), [
+      [{ "column": "a", "value": "1" }, { "column": "b", "value": "13153" }, { "column": "c", "value": "thirteen thousand one hundred fifty three" }],
+      [{ "column": "a", "value": "1" }, { "column": "b", "value": "987"   }, { "column": "c", "value": "some other number" }]
+    ]);
 
     db.close();
     return;
@@ -40,5 +35,23 @@ function test() {
   throw 'reading from the second db should have failed!';
 }
 
-test();
+function testPersistence() {
+  // Create a db with some data
+  var db = SQL.open();
+  db.exec("CREATE TABLE my_table(a INTEGER, b INTEGER, c VARCHAR(100));");
+  db.exec("INSERT INTO my_table VALUES(1,987,'some other number');");
+  db.exec("INSERT INTO my_table VALUES(5,6987,'moar numberz');");
+
+  // Serialize it to a typed array
+  var serialized = db.serialize();
+
+  var db2 = SQL.open(serialized);
+
+}
+
+// Run tests
+
+testBasics();
+
+print('ok.');
 
