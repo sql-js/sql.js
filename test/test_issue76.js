b0aa73826637297969332461267f0cbf22a4f7cd
Ophir LOJKINE <pere.jobs@gmail.com>
2014-11-26 17:17:24 +0100

    Add test for #76
    There is currently a bug in `api.coffee` that makes sql.js throw an
    error when binding a null value to a statement.

---
+++ test/test_issue76.js
@@ -0,0 +1,18 @@
+exports.test = function(sql, assert) {
+	// Create a database
+	var db = new sql.Database();
+	// Ultra-simple query
+	var stmt = db.prepare("VALUES (?)");
+	// Bind null to the parameter
+	stmt.bind([null]);
+
+	assert.deepEqual(stmt.get(), [null],
+			"binding a null value to a statement parameter");
+	db.close();
+};
+
+if (module == require.main) {
+	var sql = require('../js/sql.js');
+	var assert = require('assert');
+	exports.test(sql, assert);
+}
