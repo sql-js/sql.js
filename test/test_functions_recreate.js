2bbe51304d947d3293e7ca2a110df9dba1f15498
Shapovalov Kirill <ShapovalovKL@ShapovalovKL.geocenter-consulting.local>
2019-07-12 23:09:35 +0300

    Fix test_functions_recreate console output

--- test/test_functions_recreate.js
+++ test/test_functions_recreate.js
@@ -1,7 +1,9 @@
 exports.test = function(sql, assert) {
   // Test 1: Create a database, Register single function, close database, repeat 1000 times
+  
   for (var i = 1; i <= 1000; i++) 
   {
+    let lastStep=(i==1000);
     let db = new sql.Database();
     function add() {return i;}
     try
@@ -14,15 +16,17 @@ exports.test = function(sql, assert) {
     }
     var result = db.exec("SELECT TestFunction"+i+"()");
     var result_str = result[0]["values"][0][0];
-    assert.equal(result_str, i, "Test 1: Recreate database "+i+"th times and register function");
+    if((result_str!=i)||(lastStep))
+      assert.equal(result_str, i, "Test 1: Recreate database "+i+"th times and register function");
     db.close();
   }
   
-  // Test 2: Create a database, Register same function  1000 times, close database, repeat
+  // Test 2: Create a database, Register same function  1000 times, close database
   {
     let db = new sql.Database();
     for (var i = 1; i <= 1000; i++) 
     {
+      let lastStep=(i==1000);
       function add() {return i;}
       try
       {
@@ -34,7 +38,8 @@ exports.test = function(sql, assert) {
       }
       var result = db.exec("SELECT TestFunction()");
       var result_str = result[0]["values"][0][0];
-      assert.equal(result_str, i, "Test 2: Reregister function "+i+"th times");
+      if((result_str!=i)||(lastStep))
+        assert.equal(result_str, i, "Test 2: Reregister function "+i+"th times");
     }
     db.close();
   }
@@ -46,7 +51,7 @@ if (module == require.main) {
   const sql_loader = require('./load_sql_lib');
   sql_loader(target_file).then((sql)=>{
     require('test').run({
-      'test functions recreeate': function(assert){
+      'test creating multiple functions': function(assert){
         exports.test(sql, assert);
       }
     });
