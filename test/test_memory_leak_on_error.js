98d92fc15880eb6c2078fc64d616a76cceec9d27
Ophir LOJKINE <pere.jobs@gmail.com>
2019-11-05 11:04:13 +0100

    Update test_memory_leak_on_error.js

--- test/test_memory_leak_on_error.js
+++ test/test_memory_leak_on_error.js
@@ -1,20 +1,17 @@
 // See: https://github.com/kripken/sql.js/issues/306
 exports.test = function(sql, assert) {
-  var errors = [];
-  for (var i=0; i<10000; i++) {
+  var errors = 0, runs=10000;
+  for (var i=0; i<runs; i++) {
     var db = new sql.Database()
     try {
       db.exec("CREATE TABLE cats (name TEXT NOT NULL, age INTEGER NULL)")
       db.exec("INSERT INTO cats (name, age) VALUES (NULL, 3)")
     } catch (e) {
-      errors[i] = e;
+      errors += e.toString() === "Error: NOT NULL constraint failed: cats.name";
     }
     db.close()
   }
-  assert.equal(
-    errors.every(e => e.toString() === "Error: NOT NULL constraint failed: cats.name"),
-    true,
-    "Multiple constraint violation errors do not trigger an OOM error");
+  assert.equal(errors, runs, "Multiple constraint violation errors do not trigger an OOM error");
 };
 
 if (module == require.main) {
