ad2b8bb696dda855f6a615782e3b2bd06af4c1c2
Ophir LOJKINE <pere.jobs@gmail.com>
2019-11-05 10:45:59 +0100

    fix test_memory_leak_on_error.js

--- test/test_memory_leak_on_error.js
+++ test/test_memory_leak_on_error.js
@@ -5,15 +5,16 @@ exports.test = function(sql, assert) {
     try {
       db.exec("CREATE TABLE cats (name TEXT NOT NULL, age INTEGER NULL)")
       db.exec("INSERT INTO cats (name, age) VALUES (NULL, 3)")
+      assert.fail() // The line above should have raised an error
     } catch (e) {
-      equal(e.toString(), "Error: NOT NULL constraint failed: cats.name")
+      assert.equal(e.toString(), "Error: NOT NULL constraint failed: cats.name")
     }
     db.close()
   }
 };
 
 if (module == require.main) {
-	const target_file = process.argv[2];
+  const target_file = process.argv[2];
   const sql_loader = require('./load_sql_lib');
   sql_loader(target_file).then((sql)=>{
     require('test').run({
