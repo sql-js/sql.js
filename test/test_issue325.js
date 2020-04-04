4fca3be41d4295d64bcb01bc98d19dfded173893
Ophir LOJKINE <ophir.lojkine@auto-grid.com>
2020-04-04 21:32:24 +0200

    Update test/test_issue325.js

--- test/test_issue325.js
+++ test/test_issue325.js
@@ -3,63 +3,19 @@ exports.test = function(sql, assert){
     "use strict";
     // Create a database
     var db = new sql.Database();
-    var result;
-    var value;
 
-    // verify Infinity upper-bound
-    result = db.exec("SELECT 1.7976931348623159e+308 AS value");
-    value = result[0].values[0][0];
+    // binding a large number 
     assert.strictEqual(
-        value,
-        Infinity,
-        "SELECT 1.7976931348623159e+308 is Infinity"
-    );
-    assert.notEqual(
-        value,
+        db.exec("SELECT ?", [1.7976931348623157e+308])[0].values[0][0],
         1.7976931348623157e+308,
-        "SELECT 1.7976931348623159e+308 is Infinity"
+        "binding 1.7976931348623159e+308 as a parameter"
     );
 
     // inline result value test
-    result = db.exec("SELECT 1.7976931348623157e+308 AS value");
-    value = result[0].values[0][0];
-    assert.notEqual(
-        value,
-        Infinity,
-        "SELECT 1.7976931348623157e+308 is not Infinity"
-    );
     assert.strictEqual(
-        value,
+        db.exec("SELECT 1.7976931348623157e+308")[0].values[0][0],
         1.7976931348623157e+308,
         "SELECT 1.7976931348623157e+308 is 1.7976931348623157e+308"
-    );
-
-    // bind result value test
-    result = db.exec("SELECT ? AS value", [1.7976931348623157e+308]);
-    value = result[0].values[0][0];
-    assert.notEqual(
-        value,
-        Infinity,
-        "SELECT ? AS value is not Infinity"
-    );
-    assert.strictEqual(
-        value,
-        1.7976931348623157e+308,
-        "SELECT ? AS value is 1.7976931348623157e+308"
-    );
-
-    // CAST value test
-    result = db.exec("SELECT CAST('1.7976931348623157e+308' AS REAL) AS value");
-    value = result[0].values[0][0];
-    assert.notEqual(
-        value,
-        Infinity,
-        "SELECT CAST('1.7976931348623157e+308' AS REAL) is not Infinity"
-    );
-    assert.strictEqual(
-        value,
-        1.7976931348623157e+308,
-        "SELECT CAST('1.7976931348623157e+308' AS REAL) is 1.7976931348623157e+308"
     );
 
     // Close the database and all associated statements
@@ -81,4 +37,3 @@ if (module == require.main) {
     assert.fail(e);
   });
 }
-
