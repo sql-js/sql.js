e2b835b8e1a50193ffedc1daa05564f4e88fc7f5
lovasoa <pere.jobs@gmail.com>
2021-03-15 08:57:54 +0000

    Deploying to gh-pages from  @ 5783569103652cc788ef320182e8a16b314f4d48 🚀

--- dist/worker.sql-wasm-debug.js
+++ dist/worker.sql-wasm-debug.js
@@ -2420,9 +2420,9 @@ function updateGlobalBufferAndViews(buf) {
   Module['HEAPF64'] = HEAPF64 = new Float64Array(buf);
 }
 
-var STACK_BASE = 5309232,
+var STACK_BASE = 5310288,
     STACKTOP = STACK_BASE,
-    STACK_MAX = 66352;
+    STACK_MAX = 67408;
 
 assert(STACK_BASE % 16 === 0, 'stack must start aligned');
 
