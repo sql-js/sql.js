a927950821a5de68d673432f69e644ac22d4c300
Bill Mill <bill@billmill.org>
2022-09-06 09:43:08 -0400

    improve quantile test, add multiple agg test

--- test/test_aggregate_functions.js
+++ test/test_aggregate_functions.js
@@ -1,11 +1,13 @@
 exports.test = function (SQL, assert) {
+    function assertFloat(got, expected, message="", sigma=0.001) {
+        assert.ok(got > expected - sigma && got < expected + sigma, message);
+    }
+
     var db = new SQL.Database();
 
     db.create_aggregate(
         "sum", {
-            init: function () { return { sum: 0 }; },
-            step: function (state, value) { state.sum += value; },
-            finalize: function (state) { return state.sum; }
+            step: function (state, value) { return (state || 0) + value; },
         }
     );
 
@@ -18,28 +20,32 @@ exports.test = function (SQL, assert) {
         "percentile", {
             init: function () { return { vals: [], pctile: null }; }, // init
             step: function (state, value, pctile) {
+                state.pctile = pctile;
                 state.vals.push(value);
+                return state;
             },
             finalize: function (state) {
                 return percentile(state.vals, state.pctile);
             }
         }
     );
-    var result = db.exec("SELECT percentile(col, 20) FROM test;");
-    assert.equal(result[0].values[0][0], 1, "Aggregate function with two args");
+    result = db.exec("SELECT percentile(col, 80) FROM test;");
+    assertFloat(result[0].values[0][0], 2.6, "Aggregate function with two args");
 
     db.create_aggregate(
         "json_agg", {
-            step: function(state, val) { state.push(val); },
+            step: function(state, val) { state = (state || []); state.push(val); return state; },
             finalize: function(state) { return JSON.stringify(state); }
         }
     );
 
     db.exec("CREATE TABLE test2 (col, col2);");
     db.exec("INSERT INTO test2 values ('four score', 12), ('and seven', 7), ('years ago', 1);");
-    var result = db.exec("SELECT json_agg(col) FROM test2;");
-    console.log("output: ", result[0].values);
+    result = db.exec("SELECT json_agg(col) FROM test2;");
     assert.deepEqual(JSON.parse(result[0].values[0]), ["four score", "and seven", "years ago"], "Aggregate function that returns JSON");
+
+    result = db.exec("SELECT json_agg(col), json_agg(col2) FROM test2;");
+    assert.deepEqual(result[0].values[0].map(JSON.parse), [["four score", "and seven", "years ago"], [12, 7, 1]], "Multiple aggregations at once");
 }
 
 // helper function to calculate a percentile from an array. Will modify the
