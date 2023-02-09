a161ff40dc0bd7d7e48729a24e910cd56e24aed7
Jānis Elmeris <janise@inbox.lv>
2023-02-09 18:33:27 +0200

    Fix grammar (#542)

--- src/api.js
+++ src/api.js
@@ -649,7 +649,7 @@ Module["onRuntimeInitialized"] = function onRuntimeInitialized() {
         return true;
     };
 
-    /** Reset a statement, so that it's parameters can be bound to new values
+    /** Reset a statement, so that its parameters can be bound to new values
     It also clears all previous bindings, freeing the memory used
     by bound parameters.
      */
