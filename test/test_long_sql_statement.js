ea72013970b5e83c1976135fe15af18c98dc25ed
lovasoa <pere.jobs@gmail.com>
2024-01-20 22:55:23 +0000

    add a test for #561

---
+++ test/test_long_sql_statement.js
@@ -0,0 +1,35 @@
+// test for https://github.com/sql-js/sql.js/issues/561
+exports.test = function (sql, assert) {
+    // Create a database
+    var db = new sql.Database();
+    var len = 70000;
+    var many_a = "";
+    for (var i = 0; i < len; i++) many_a += 'a';
+
+    console.log('xxx');
+    var res = db.exec("select length('" + many_a + "') as len");
+    var expectedResult = [
+        {
+            columns: ['len'],
+            values: [
+                [len]
+            ]
+        }
+    ];
+    assert.deepEqual(res, expectedResult, "length of long string");
+};
+
+if (module == require.main) {
+    const target_file = process.argv[2];
+    const sql_loader = require('./load_sql_lib');
+    sql_loader(target_file).then((sql) => {
+        require('test').run({
+            'test long sql string (issue 561)': function (assert) {
+                exports.test(sql, assert);
+            }
+        });
+    }).catch((e) => {
+        console.error(e);
+        assert.fail(e);
+    });
+}
